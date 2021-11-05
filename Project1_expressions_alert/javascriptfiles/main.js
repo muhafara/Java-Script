02652fc06bee56a77ff38cef4abfe46fdcd418a5
muhafara <faranmuhammad@hotmail.com>
2021-11-05 12:40:59 +0000

    First Assignment Subsmission
    First Assignment submission
    Minimum project requirements:
    
    •  Use the window.alert() and document.write() methods.
    •  Assign string values to two variables and concatenate the variables together.
    •  Assign a color to at least one variable using the fontcolor() method.
    •  Write an expression.
    •  Add comments throughout your JavaScript explaining your code.

---
+++ Project1_expressions_alert/javascriptfiles/main.js
@@ -0,0 +1,34 @@
+
+/*============================================================
+Function for windows alert
+======================================================*/
+function windows_Alert_Function(){
+    var my_Windows_Alert = "Hey, I,m using windows.Alert here....!";
+    window.alert(my_Windows_Alert);
+    var result;
+    document.getElementsByClassName("windows_Alert_Function").innerHtml = result;
+}
+
+
+/*============================================================
+String concatenation
+======================================================*/
+
+var my_String_one = "hello, I'm";
+var my_String_two = " concatenating two string";
+var result_Of_Concatenation = my_String_one + my_String_two;
+
+/*============================================================
+Changing font color of a concatenate string
+======================================================*/
+result_Of_Concatenation = result_Of_Concatenation.fontcolor("red");
+
+/*============================================================
+document.write()
+======================================================*/
+
+document.write(result_Of_Concatenation);
+
+
+
+
