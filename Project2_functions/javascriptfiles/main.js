5635caa973b1dbf987d425100da6073a565766d9
muhafara <faranmuhammad@hotmail.com>
2021-11-05 13:09:49 +0000

    Project_Two_Subsmission_Function
    Assignment two subsmission.
    
    Minimum project requirements:
    
    •  Write a function that uses the += operator
    •  Create an HTML element to call and display your function
    •  Add comments throughout your JavaScript explaining your code

---
+++ Project2_functions/javascriptfiles/main.js
@@ -0,0 +1,22 @@
+/*========================================================================
+Function using += to concatenate
+=========================================================================*/
+
+function concatination_Strings(){
+    var my_String ="Hi, My is "
+    my_String +=  "Faran";
+    document.getElementById("concatinating_string").innerHTML = my_String;
+}
+
+/*========================================================================
+Function
+=========================================================================*/
+/*function myVariable(){
+    var my_Var_One = "Hi";
+    var_my_Var_two =  "Whats's up";
+    var result = my_Var_One + var_my_Var_two;
+    document.getElementById("my_Variable").innerHTML=result;
+}*/
+
+
+
