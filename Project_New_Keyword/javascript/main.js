53048fe35cab428157c9d1e62828c403c86fc5e6
muhafara <faranmuhammad@hotmail.com>
2021-11-08 11:29:51 +0000

    Assignment 10
    Minimum project requirements:
    
    Write a function that use a while loop
    Write a function that use a for loop
    Write a function that use an array
    Create an object using the “let” keyword
    Write any HTML elements needed to call and display your JavaScript code
    Add comments throughout your JavaScript explaining your code

--- Project_New_Keyword/javascript/main.js
+++ Project_New_Keyword/javascript/main.js
@@ -1,22 +1,26 @@
 class Vehicle{ /*Defining a class*/
 
-     Vehicle(Make, Model, Year, Color){ /*Constructor*/
+     constructor(Make, Model, Year, Color){ /*Constructor*/
         this.Vehicle_Make = Make;
         this.Vehicle_Model = Model;
         this.Vehicle_Year = Year;
         this.Vehicle_Color = Color;
     }
+    
+}
+
 
     /*creating an object using new keyword*/
-     Jack = new Vehicle("MERCEDES", "A35", 2020, "RED");
+    var Jack = new Vehicle("MERCEDES", "A35", 2020, "RED");
 
- myFunction(){
+
+ function myFunction(){
+
+    
     document.getElementById("keyandcon").innerHTML = "Jack Drives a " + Jack.Vehicle_Make    
      + " Model " + Jack.Vehicle_Model +" Year of Make "+ Jack.Vehicle_Year + "Colour "
-    + Jack.Vehicle_Color;; 
+    + Jack.Vehicle_Color;
  }
-
-}
 
 
 /*===================================================================
