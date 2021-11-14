474368d818fdb5678d0f3c5a82c50c856a165ce4
muhafara <faranmuhammad@hotmail.com>
2021-11-14 12:44:19 +0000

    new assignment

--- Project6_ternary__operators_constructors/javascript/main.js
+++ Project6_ternary__operators_constructors/javascript/main.js
@@ -1,15 +1,10 @@
-/*function ternaryOperator(){
-    get_Age = document.getElementById("get_Age").value;
-    var show_Result = (get_Age < 18) ? "You are not old enough to vote" : "You can vote!";
-    document.getElementById("show_Result").innerHTML = "Result: " + show_Result;
-}*/
-
+//Function to check vote eligibility using ternanry operator, nested function
 function ternaryOperator(){
-     get_Age = 17;
-     document.getElementById("get_Age").innerHTML = show_Result();
-   
-    function show_Result(){
-    var show_result = (get_Age < 18) ? "You are not old enough to vote" : "You can vote!";
-    return show_result;
+    var get_Age = document.getElementById("get_Age").value;
+    document.getElementById("show_Result").innerHTML = show_Result();
+    //nested function
+   function show_Result(show_result){
+   var show_result = (get_Age < 18) ? "You are not old enough to vote" : "You can vote!";
+   return show_result;
 }
 }
