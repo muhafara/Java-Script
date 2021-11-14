ef0baba73de71d024e8fddd18869a20e04db754e
muhafara <faranmuhammad@hotmail.com>
2021-11-14 14:13:48 +0000

    updates

---
+++ Project9_countdown_slideshow/javascriptfiles/countdown.js
@@ -0,0 +1,11 @@
+function countdown(){
+    var seconds = document.getElementById("seconds").value;
+    function tick(){
+      seconds = seconds - 1;
+      setTimeout(tick, 1000);
+      if(seconds == -1){
+        alert("Time's up!")
+      }
+    }
+    tick();
+  }
